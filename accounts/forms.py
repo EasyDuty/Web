fdb531514fd03d19a37c3c8078b34c5cdd6973c0
Elgcha <vkdlfl681@gmail.com>
2021-10-19 16:52:37 +0900

    bug edit

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,9 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 
 class NurseForm(UserCreationForm):
-    name = forms.CharField(max_length=100)
-    age = forms.IntegerField()
-    career = forms.IntegerField()
-    ward = forms.CharField(max_length=100)
-    team = forms.CharField(max_length=100)
-    hospital = forms.CharField(max_length=100)
+
 
     class Meta:
         model = get_user_model()
@@ -24,7 +19,7 @@ class NurseForm(UserCreationForm):
             'hospital',
         )
 
-class NurseChangeForm(NurseForm):
+class NurseChangeForm(UserChangeForm):
 
     class Meta:
         model = get_user_model()
